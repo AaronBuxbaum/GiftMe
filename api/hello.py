bfede18a721caa38549a68a8ee5840af39d81ddf
Aaron Buxbaum <abuxbaum@abuxbaum-macbookpro2.roam.corp.google.com>
2017-03-15 17:59:39 -0400

    Add static hosting via Flask

--- api/hello.py
+++ api/hello.py
@@ -1,9 +1,18 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='../build')
+
+
+@app.route('/')
+def root():
+  return app.send_static_file('index.html')
+
 
 @app.route("/api/test")
 def test():
     return "Testing"
+
 
 if __name__ == "__main__":
     app.run()
