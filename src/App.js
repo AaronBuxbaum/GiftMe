5121fd7517464d5b43919a13175195eccf3f5331
Aaron Buxbaum <me@aaronbuxbaum.com>
2017-03-15 21:40:34 -0400

    Make some adjustments

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to HELL</h2>
+          <h2>Welcome to Hell</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get rolling, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
